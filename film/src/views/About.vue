d2a1fd986e9c02163ff673c9c912f06ba4897a43
Neiso <djulian.ouatla@outlook.com>
2020-09-17 14:17:06 +0200

    Adding my cv in the about page.

--- film/src/views/About.vue
+++ film/src/views/About.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <v-img src="../assets/Ouatla Djulian-page-001.jpg" id="cv"></v-img>
   </div>
 </template>
+
+<style lang="scss">
+#cv {
+  width: 30%;
+  margin: auto;
+}
+</style>
